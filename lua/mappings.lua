3ab36bbca9700721fe2ddfdf227cea1d248965d5
canta2899 <andreacantarutti@icloud.com>
2025-11-09 14:31:08 +0100

    feat: added customizations

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,17 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "d", '"_d')
+map("v", "d", '"_d')
+map("n", "<C-u>", "<C-u>zz")
+map("n", "<C-d>", "<C-d>zz")
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+map("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+    expr = true,
+    replace_keycodes = false,
+})
+vim.g.copilot_no_tab_map = true
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
