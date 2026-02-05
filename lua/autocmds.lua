022b909938c867823f89d4e0edf3dc91baa8cc26
canta2899 <andreacantarutti@icloud.com>
2026-02-06 00:32:28 +0100

    feat: auto read and git blame

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,8 @@
 require "nvchad.autocmds"
+
+-- https://stackoverflow.com/a/74230727
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
